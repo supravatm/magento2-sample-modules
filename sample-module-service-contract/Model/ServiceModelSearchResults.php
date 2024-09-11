87256025739b7214d282cb73932c79470382f03c
Supravat Mondal <supravat.com@gmail.com>
2024-09-11 13:40:07 +0530

    latest updates

--- sample-module-service-contract/Model/ServiceModelSearchResults.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Certification\ServiceContract\Model;
-use \Magento\Framework\Api\SearchResults;
-use \Certification\ServiceContract\Api\Data\ServiceModelSearchResultsInterface;
-class SearchModelSearchResults extends SearchResults implements ServiceModelSearchResultsInterface
-{
-}
