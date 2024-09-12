85fe998451546b7932f13d42a1b643870262cc31
Supravat Mondal <supravat.com@gmail.com>
2024-09-12 21:04:04 +0530

    Update Module#02

---
+++ 02.sample-WebFlow-module/Block/FirstPage.php
@@ -0,0 +1,15 @@
+<?php
+namespace SMG\WebFlow\Block;
+
+class FirstPage extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+
+    public function sayHello()
+	{
+		return __('Hello World');
+	}
+}
